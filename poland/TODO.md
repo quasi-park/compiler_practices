30dff4a5076e938bc8c83efa40cf6cfe54fc1ecf
Keita Suzuki <keitasuzuki.park@sslab.ics.keio.ac.jp>
2018-05-18 11:50:39 +0900

    Added new funcs and more to poland

--- poland/TODO.md
+++ poland/TODO.md
@@ -6,3 +6,8 @@
     - UPDATE: currently using `fgets` as input source (I dare say this is not a good idea... still have to think alittle bit more).
 - when `(` or `)` token are used after/before any other operations, this program will stop b.c. it is currently not allowing operation token right after another operation token.
 - there is (obviously) a maximmum BUFLEN right now. This is actually not that great...(although as long as Im using C, this problem is unsolveable...)
+
+## Changes I might make
+- currently using bi-directional list. might use linear list. 
+- many many unneeded vars.
+- currently using too much main. Should divide to seperate funcs.
--- poland/poland.c
+++ poland/poland.c
@@ -19,7 +19,7 @@ struct nodes {
     short numCount;
     char dat[MAXBUF];
     struct nodes *next, *prev; //pointer for list
-    struct nodes *left, *right; //pointer for tree`
+    struct nodes *left, *right; //pointer for tree
 };
 
 struct opp {
@@ -37,12 +37,13 @@ struct opp {
 
 int get_type(char *, int);
 int num_div(char *, int, struct nodes *);
+int get_priority(char c);
 
 int main(int argc, char *argv[])
 {
     char c, buf[BUFSIZE];
-    int i, prevtype;
-    struct nodes head, *new_node;
+    int i, prevtype, prev_priority, cur_priority;
+    struct nodes head, tail, *new_node, *prev_token, *loop_node;
  
 //    scanf("%s", buf);
     fgets(buf, BUFSIZE, stdin);
@@ -50,8 +51,14 @@ int main(int argc, char *argv[])
     /*Initialize header*/
     head.numCount = 0;
     *head.dat = '(';
-    head.next = head.prev = &head;
+    head.next = head.prev = &tail;
     head.left = head.right = 0;
+
+    /*Initialize tail*/
+    tail.numCount = 0;
+    *tail.dat = ')';
+    tail.next = tail.prev = &head;
+    tail.left = tail.right = 0;
 
     prevtype = OPERATION;
     for (i = 0; buf[i] != 0; i++) {
@@ -65,10 +72,11 @@ int main(int argc, char *argv[])
                 }
                 new_node = (struct nodes *)malloc(sizeof(struct nodes));
                 i = num_div(buf, i, new_node);
-                (head.prev)->next = new_node;
-                new_node->prev = head.prev;
-                new_node->next = &head;
-                head.prev = new_node;
+                (tail.prev)->next = new_node;
+                new_node->prev = tail.prev;
+                new_node->next = &tail;
+                tail.prev = new_node;
+                new_node->left = new_node->right = 0;
                 prevtype = NUMBER;
                 break;
             case OPERATION:
@@ -81,10 +89,11 @@ int main(int argc, char *argv[])
                 new_node->dat[0] = buf[i];
                 new_node->dat[1] = '\0';
                 new_node->numCount = 0;
-                (head.prev)->next = new_node;
-                new_node->prev = head.prev;
-                new_node->next = &head;
-                head.prev = new_node;
+                (tail.prev)->next = new_node;
+                new_node->prev = tail.prev;
+                new_node->next = &tail;
+                new_node->left = new_node->right = 0;
+                tail.prev = new_node;
                 prevtype = OPERATION;
                 break;
             case SPACE:
@@ -92,16 +101,45 @@ int main(int argc, char *argv[])
                 break;
             case OTHERS:
                 fprintf(stderr, "found invalid experssion %c. Unable to continue.\n Exiting\n", buf[i]);
-                free(new_node);
                 exit(1);
             default:
                 break;
         }
     }
 
-    for(new_node = head.next; new_node != &head; new_node = new_node->next){
-        printf("%s\n", new_node->dat);
+    /* check token and add ( ) nodes*/
+    for(loop_node = head.next, prev_token = &head, prev_priority = PRY_LOW;
+            loop_node != &head; loop_node = loop_node->next){
+        if(loop_node->numCount > 0)
+            continue;
+        if((cur_priority = get_priority(loop_node->dat[0])) > prev_priority){
+            new_node = (struct nodes *)malloc(sizeof(struct nodes));
+            *new_node->dat = '(';
+            new_node->numCount = 0;
+            (prev_token->next)->prev = new_node;
+            new_node->next = prev_token->next;
+            prev_token->next = new_node;
+            new_node->prev = prev_token;
+        }else if(cur_priority < prev_priority){
+            new_node = (struct nodes *)malloc(sizeof(struct nodes));
+            *new_node->dat = ')';
+            new_node->numCount = 0;
+            (loop_node->prev)->next = new_node;
+            new_node->prev = loop_node->prev;
+            loop_node->prev = new_node;
+            new_node->next = loop_node;
+        }
+
+        prev_token = loop_node;
+        prev_priority = cur_priority;
     }
+
+    /*debug loop*/
+    for(loop_node = head.next;loop_node != &head; loop_node = loop_node->next){
+        printf("%s [%s]\n", loop_node->dat, loop_node->numCount > 0 ? "num":"opp");
+    }
+ 
+    /*TODO: create trees*/
     return 0;
 }
 
@@ -123,13 +161,20 @@ int get_type(char * buf, int i){
 }
 
 int num_div(char * buf, int i, struct nodes * tgt){
-    int j;
-    j = 0;
-    while('0' <= buf[i] && buf[i] <= '9' && i <= strlen(buf)){
-        tgt->dat[j] = buf[i];
+    while('0' <= buf[i] && buf[i] <= '9'){
+        tgt->dat[tgt->numCount++] = buf[i];
         i++;
-        j++;
     }
-    tgt->dat[j] = '\0';
+    tgt->dat[tgt->numCount] = '\0';
     return i - 1;
+}
+
+int get_priority(char c){
+    struct opp * p;
+    for (p = opp_ary; p->op_char != 0; p++) {
+        if(p->op_char == c){
+            return p->priority;
+        }
+    }
+    return -1;
 }
